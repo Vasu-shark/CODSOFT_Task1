772070e5036122d1d354f51bd13cde2b39cd0a08
Vasu Sharma <svasu0780@gmail.com>
2024-08-09 11:04:45 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+first = int(input("enter the first number : "))
+operator = input("enter operator (+,-,/,*,**) : ")
+second =int(input("enter the second number : "))
+if operator == "+":
+    print(first+second)
+elif operator == "-":
+    print(first-second)
+elif operator =="/":
+    print(first%second)
+elif operator=="*":
+    print(first*second)
+elif operator =="**":
+    print(first**second)
+else:
+    print("Invalid operator")
+
+
+
+
